79a3f6b73cc80c8709837004fe1d9a02b4125c3b
pr540 <praneethssr.2002@gmail.com>
2025-07-23 15:03:47 +0530

    add all files into github repo

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ padding: '2rem', fontFamily: 'Arial' }}>
+      <h1>Hello from Frontend 👋</h1>
+      <p>This is a working React app.</p>
+    </div>
+  );
+}
+
+export default App;
